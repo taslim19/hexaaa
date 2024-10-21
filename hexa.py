57193839269c2dd9ea0a69b5ef42837ed05b5d93
Drag <161552347+taslim19@users.noreply.github.com>
2024-10-21 13:55:09 +0530

    Update hexa.py

--- hexa.py
+++ hexa.py
@@ -1,105 +1,106 @@
-from telethon import events, TelegramClient
+from telethon import events , TelegramClient
 from asyncio import sleep as zzz
 from random import randint
+from re import match
 
-# Don't edit else gay this constant
+#dont edit else gay this constant
 api_id = 2282111
 api_hash = 'da58a1841a16c352a2a999171bbabcad'
-bot = TelegramClient('session', api_id, api_hash)
+bot = TelegramClient('session' , api_id , api_hash)
 chat = 572621020
 
-# Edit the list
-pokemon_list = ["Charizard", "Blastoise", "Beedrill", "Alakazam", "Gengar", "Kangaskhan", "Pinsir", "Gyarados", "Aerodactyl", "Mewtwlix", "Scizor", "Heracross", "Houndoom", "Tyranitar", "Sceptile", "Swampert", "Gardevoir", "Aggron", "Manectric", "Salamence", "Metagross", "Latias", "Latios", "Rayquaza", "Lopunny", "Garchomp", "Lucario", "Gallade", "Golisopod", "Wimpod", "Greninja", "Froakie", "Hawlucha", "Jellicent", "Arrokuda", "Barraskewda", "Noivern", "Noibat", "Chandelure", "Lampent", "Darmanitan", "Staraptor", "Primarina", "Dwebble", "Crustle", "Floette", "Florges", "Litwick", "Dreepy", "Slaking", "Slakoth", "Vigoroth", "Snorlax", "Munchlax", "Talonflame", "Fletchling", "Sneasel", "Scyther", "Metang", "Beldum", "Golem", "Drakloak", "Cosmog", "Cosmoem", "Inteleon ", "Charmander ", "Riolu", "Dragapult "]
-
-# Add your private group ID here
-private_chat_id = '-1002151109829'
-
-@bot.on(events.NewMessage(outgoing=True, pattern='/go'))
+#edit the list
+list = ["Charizard", "Blastoise",  "Alakazam", "Gengar", "Kangaskhan", "Pinsir", "Gyarados", "Aerodactyl", "Mewtwo", "Houndoom", "Sceptile", "Gardevoir", "Salamence", "Metagross", "Latias", "Latios", "Rayquaza", "Lopunny", "Lucario", "Gallade", "Wimpod", "Greninja", "Froakie", "Hawlucha", "Frillish", "Arrokuda", "Darmanitan", "Primarina", "Popplio", "Floette", "Florges", "Dreepy", "Sneasler", "Starly", "Espeon", "Jolteon", "Mudkip", "Squirtle", "Rotom", "Drakloak", "Inteleon"]
+@bot.on(events.NewMessage(outgoing=True,pattern='/go'))
 async def begin(event):
     global hunt
     hunt = True
-    x = await bot.send_message(chat, "/hunt")
-    try:
-        async with bot.conversation('@Hexamonbot') as conv:
-            await conv.get_response(x.id)
+    x = await bot.send_message(chat , "/hunt")
+    try:  
+     async with bot.conversation('@Hexamonbot') as conv:
+       await conv.get_response(x.id)
     except:
-        await zzz(3, 5)
-        await bot.send_message(chat, "/hunt")
-    for i in range(5, 10000):
-        await zzz(randint(5000, 6020))
-        await bot.send_message(chat, "/hunt")
+       await zzz(3,5)
+       await bot.send_message(chat , "/hunt")
+    for i in range(5,10000):
+      await zzz(randint(5000, 6020))
+      await bot.send_message(chat , "/hunt")
 
-@bot.on(events.NewMessage(chats=chat, incoming=True))
+@bot.on(events.NewMessage(chats=chat,incoming=True))
 async def hunt(event):
-    global hunt
-    if hunt:
-        text = event.message.text
-        message = await bot.get_messages(chat, ids=event.message.id)
+    if hunt == True:
+     text = event.message.text
+     hun = True
+     message = await bot.get_messages(chat, ids=event.message.id)
+     if ("Shiny" in text):
+        bot.disconnect()
+     elif("TM" in text):
+        print(event.message.text)
+        await zzz(randint(3,5))
+        x = await bot.send_message(chat , "/hunt")
+        try:  
+         async with bot.conversation('@Hexamonbot') as conv:
+           await conv.get_response(x.id)
+        except:
+           await zzz(5,6)
+           await bot.send_message(chat , "/hunt")
+     elif any(item in text for item in list):
+        await message.click(text="Battle")
+        await message.click(text="Battle")
+        await message.click(text="Battle")
+     elif("A wild" in text or "An expert" in text):
+      if hun == False:
+       pass
+      else:
+       await zzz(randint(3,5))
+       x = await bot.send_message(chat , "/hunt")
+       try:  
+        async with bot.conversation('@Hexamonbot') as conv:
+          await conv.get_response(x.id)
+       except:
+          await zzz(3,5)
+          await bot.send_message(chat , "/hunt")
+      
 
-        # Check for shiny Pokémon
-        if "Shiny" in text:
-            await bot.send_message(private_chat_id, "✨ Shiny Pokémon found! @dragtf")  # Tag you if needed
-            bot.disconnect()
-        elif "TM" in text:
-            print(event.message.text)
-            await zzz(randint(3, 5))
-            x = await bot.send_message(chat, "/hunt")
-            try:
-                async with bot.conversation('@Hexamonbot') as conv:
-                    await conv.get_response(x.id)
-            except:
-                await zzz(5, 6)
-                await bot.send_message(chat, "/hunt")
-        elif any(item in text for item in pokemon_list):
-            await message.click(text="Battle")
-            await message.click(text="Battle")
-            await message.click(text="Battle")
-        elif "A wild" in text or "An expert" in text:
-            await zzz(randint(3, 5))
-            x = await bot.send_message(chat, "/hunt")
-            try:
-                async with bot.conversation('@Hexamonbot') as conv:
-                    await conv.get_response(x.id)
-            except:
-                await zzz(3, 5)
-                await bot.send_message(chat, "/hunt")
-
-@bot.on(events.NewMessage(chats=chat, incoming=True))
-async def battle(event):
-    if event.message.text[:13] == "Battle begins":
+@bot.on(events.NewMessage(chats=chat,incoming=True))
+async def hunt(event):
+   print(event.message.text)
+   if event.message.text[:13] == "Battle begins":
         message = await bot.get_messages(chat, ids=event.message.id)
         await zzz(21)
-        await message.click(text="Poke Balls")
-        await message.click(text="Poke Balls")
-        await message.click(text="Poke Balls")
+        await message.click(text = "Poke Balls")
+        await message.click(text = "Poke Balls")
+        await message.click(text = "Poke Balls")      
+
 
 @bot.on(events.MessageEdited(chats=chat))
-async def catcher(event):
-    message = await bot.get_messages(chat, ids=event.message.id)
-    await message.click(text="Poke Balls")
-    await message.click(text="Poke Balls")
-    await message.click(text="Poke Balls")
-    
-    if event.message.text[:4] == "Wild":
-        await zzz(1)
-        await message.click(text="Regular")
-        await message.click(text="Regular")
-        await message.click(text="Regular")
-        
-    if any(keyword in event.message.text for keyword in ['fled', 'fainted', 'caught']):
-        await zzz(randint(3, 5))
-        x = await bot.send_message(chat, "/hunt")
-        try:
-            async with bot.conversation('@Hexamonbot') as conv:
-                await conv.get_response(x.id)
-        except:
-            await zzz(3, 5)
-            await bot.send_message(chat, "/hunt")
+async def cacther(event):
+   message = await bot.get_messages(chat, ids=event.message.id)
+   await message.click(text = "Poke Balls")
+   await message.click(text = "Poke Balls")
+   await message.click(text = "Poke Balls") 
+   if event.message.text[:4] == "Wild":
+      await zzz(1)
+      await message.click(text = "Regular")
+      await message.click(text = "Regular")
+      await message.click(text = "Regular")
+   if any(keyword in event.message.text for keyword in ['fled', 'fainted', 'caught']):
+      await zzz(randint(3,5))
+      x = await bot.send_message(chat , "/hunt")
+      try:  
+       async with bot.conversation('@Hexamonbot') as conv:
+         await conv.get_response(x.id)
+      except:
+         await zzz(3,5)
+         await bot.send_message(chat , "/hunt")
 
-@bot.on(events.NewMessage(outgoing=True, pattern='/stop'))
+
+
+@bot.on(events.NewMessage(outgoing=True,pattern='/stop'))
 async def stop(event):
     global hunt
     hunt = False
+
 
 bot.start()
 bot.run_until_disconnected()
